00c921e98c4d8b6f316fb896ca2807bff14c5332
AfreenMunshi <munshi.afreen@gmail.com>
2014-02-23 00:29:55 +0530

    adding route file for credit card processing

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ CF::Application.routes.draw do
 get 'tags/:tag', to: 'campaigns#index', as: :tag
 
 post '/store_bank_account', to: 'balanced#store_bank_account', as: :store_bank_account
+post '/store_credit_card', to: 'balanced#store_credit_card', as: :store_credit_card 
 get '/new_card', to: 'users#new_card'
+get '/credit_card_form', to: 'transactions#credit_card_form'
 
   # get '/transactions/pending', to: 'transactions#pending', as: :pending_transactions
 
